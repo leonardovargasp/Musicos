e34b39e69c05feb448b866e3cfa248f8832c4f26
leonardovargasp <124205211+leonardovargasp@users.noreply.github.com>
2023-03-22 19:59:07 +0000

    nuevo

---
+++ musicos/Program.cs
@@ -0,0 +1,47 @@
+﻿class musico
+{
+    public string Nombre{ get;set;}
+    public musico(string n){Nombre = n;}
+    public void saluda (){Console.WriteLine($"Hola soy {Nombre}");}
+    public virtual void Tocar() {Console.WriteLine($"{Nombre}tocando su instrumento");}
+}
+ class bajista:musico
+{
+    public string bajo {get;set;}
+    public bajista (string n,string b):base(n)
+ {
+    bajo=b;
+ }
+public override void Tocar()
+  {
+    Console.WriteLine($"{Nombre}tocando su {bajo}");
+  }
+
+ }
+
+ class baterista:musico
+ {
+    public string bateria{get;set;}
+    public baterista
+ }
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<musico> Galgos = new List<musico>();
+        Galgos.Add(new musico("Manuel Medrano"));
+        Galgos.Add(new bajista("EL pa","Musicman"));
+
+        foreach(var m in Galgos)m.saluda();
+        foreach(var m in Galgos)m.Tocar();
+
+       
+        
+        
+        
+
+
+    }
+}
