7d454ea053636a3a21b6bccdecfa19e26ef1fdcd
leonardovargasp <124205211+leonardovargasp@users.noreply.github.com>
2023-03-23 19:32:33 +0000

    abstract music

--- musicos/Program.cs
+++ musicos/Program.cs
@@ -1,9 +1,11 @@
-﻿class musico
+﻿abstract class musico
 {
     public string Nombre{ get;set;}
     public musico(string n){Nombre = n;}
     public void saluda (){Console.WriteLine($"Hola soy {Nombre}");}
-    public virtual void Tocar() {Console.WriteLine($"{Nombre}tocando su instrumento");}
+    public /*virtual*/ abstract void Tocar(); 
+    
+    //{Console.WriteLine($"{Nombre}tocando su instrumento");}
 }
  class bajista:musico
 {
@@ -12,7 +14,7 @@
  {
     bajo=b;
  }
-public override void Tocar()
+public override /*new*/ void Tocar()
   {
     Console.WriteLine($"{Nombre}tocando su {bajo}");
   }
@@ -22,7 +24,15 @@ public override void Tocar()
  class baterista:musico
  {
     public string bateria{get;set;}
-    public baterista
+    public baterista (string n,string b, string bt):base(n){
+        bateria=bt;
+    }
+
+    public override /*new*/ void Tocar()
+  {
+    Console.WriteLine($"{Nombre}tocando su {bateria}");
+  }
+
  }
 
 
@@ -31,7 +41,7 @@ internal class Program
     private static void Main(string[] args)
     {
         List<musico> Galgos = new List<musico>();
-        Galgos.Add(new musico("Manuel Medrano"));
+        //Galgos.Add(new musico("Manuel Medrano"));//
         Galgos.Add(new bajista("EL pa","Musicman"));
 
         foreach(var m in Galgos)m.saluda();
